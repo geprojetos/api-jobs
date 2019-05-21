90f58560b22210c93e24955f439955f636c8a131
geprojetos <geprojetosbrasil@gmail.com>
2019-05-21 17:49:55 -0300

    Ajustando o login com bcrypjs

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -2,60 +2,64 @@ const mongoose      = require('mongoose');
 const adminModel    = mongoose.model('admin');
 const jwt           = require('jsonwebtoken');
 const authSecret    = require('../config/auth.secret.json');
+const bcryptjs      = require('bcryptjs');
 let apiLogin    = {};
 
 apiLogin.login = async (req, res) => {
 
     try {
         const { login, password } = req.body;
+        const user = await adminModel.findOne({ login });
 
-        await adminModel.findOne({ login, password }, (error, user) => {
+        if(!login) {
+            console.log('login não informado');
+            res.status(400).json({ fail: 'login não informado' });
+            return;
+        };
 
-            if(error) {
-                console.log(error.message);
-                res.status(400).json(error.message);
-                return
-            };
+        if(!user) {
+            console.log('############### login inválido ###############');
+            res.status(400).json({fail: 'login inválido'});
+            return;
+        };
 
-            if(!login) {
-                console.log('login não informado');
-                res.status(400).json({ fail: 'login não informado' });
-                return;
-            };
+        if(!password) {
+            console.log('password invalído');
+            res.status(400).json({ fail: 'password invalído' });
+            return;
+        };
 
-            if(!password) {
-                console.log('password não informado');
-                res.status(400).json({ fail: 'password não informado' });
-                return;
-            };
+        if(!await bcryptjs.compare(password, user.password)) {
+            
+            console.log('password incorreto');
+            res.status(400).json({ fail: 'password incorreto' });
+            return;
+        };
 
-            if(user) {
-                user.password = undefined;
+        if(user) {
 
-                const token = jwt.sign(
-                    {
-                        id: user._id,
-                        login: user.login,
-                        email: user.email
-                    },
-                    authSecret.secret,
-                    {
-                        expiresIn: 86400
-                    }
-                );
-                
-                res.set('x-access-token', token);
-                res.status(200).json({ user, token });
+            user.password = undefined;
+    
+            const token = jwt.sign(
+                {
+                    id: user._id,
+                    login: user.login,
+                    email: user.email
+                },
+                authSecret.secret,
+                {
+                    expiresIn: 86400
+                }
+            );
+            
+            res.set('x-access-token', token);
+            res.status(200).json({ user, token });
+    
+            console.log('############# Logado ###############');
+            console.log(user);
+            console.log('####################################');
+        };
 
-                console.log('############# Logado ###############');
-                console.log(user);
-                console.log('####################################');
-                return;
-            } else {
-                console.log('login ou password inválidos');
-                res.status(400).json({fail: 'login ou password inválidos'});
-            };
-        })      
     } catch (error) {
         console.log(error.message);
         res.status(400).json({ fail: error.message })
