b5a29a197c3023bb9815638797cfb5e5b3573f50
geprojetos <geprojetosbrasil@gmail.com>
2019-06-12 14:36:52 -0300

    Adicionando cors ao projeto

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const server    = require('http').Server(app);
 const io        = require('socket.io')(server);
 const consign   = require('consign');
 const bodyParser= require('body-parser');
+const cors      = require('cors');
 const port      = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     
